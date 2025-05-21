709931b88b16eeb91b5074bf91b42406a2587d3c
Salahidine Lemaachi <salahidine.lemaachi@datadoghq.com>
2025-05-21 11:08:00 +0200

    adding script to download artifacts

--- toto/download_artifacts.py
+++ toto/download_artifacts.py
@@ -1,9 +1,28 @@
-from data.util.s3 import copy_dir_from_s3
-
-from global_settings import BUCKET, MODEL_KEY, DATASET_KEY, LOCAL_MODEL_DIR, LOCAL_DATASET_DIR
-
-# Download the model and dataset from S3
-copy_dir_from_s3(BUCKET, MODEL_KEY, LOCAL_MODEL_DIR)
-copy_dir_from_s3(BUCKET, DATASET_KEY, LOCAL_DATASET_DIR)
+import os
+import zipfile
+import urllib.request
+from global_settings import MODEL_KEY, DATASET_KEY, TARGET_DIR
 
 
+def download_file(url, dest_path):
+    print(f"Downloading {url}...")
+    urllib.request.urlretrieve(url, dest_path)
+    print(f"Saved to {dest_path}")
+
+def unzip_file(zip_path, extract_to):
+    print(f"Unzipping {zip_path} to {extract_to}...")
+    os.makedirs(extract_to, exist_ok=True)
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    print("Unzip complete.")
+
+def main():
+    for url in [MODEL_KEY, DATASET_KEY]:
+        filename = os.path.basename(url)
+        local_zip = os.path.join(os.getcwd(), filename)
+
+        download_file(url, local_zip)
+        unzip_file(local_zip, TARGET_DIR)
+
+if __name__ == "__main__":
+    main()
--- toto/global_settings.py
+++ toto/global_settings.py
@@ -1,8 +1,11 @@
 
-BUCKET = ""
-MODEL_KEY = ""
-LOCAL_MODEL_DIR = "temp/open_source_toto_base"
+import os 
 
-DATASET_KEY = ""
-LOCAL_DATASET_DIR = "temp/BOOM"
+TARGET_DIR = os.path.join(os.getcwd(), "artifacts")
+
+MODEL_KEY = "https://origin-static-assets.s3.us-east-1.amazonaws.com/download/Toto-Open-Base-1.0.zip"
+LOCAL_MODEL_DIR = os.path.join(TARGET_DIR, "Toto-Open-Base-1.0")
+
+DATASET_KEY = "https://origin-static-assets.s3.us-east-1.amazonaws.com/download/BOOM.zip"
+LOCAL_DATASET_DIR = os.path.join(TARGET_DIR, "BOOM")
 
